24d968e5feb11e2e2c4480fb9c0fe08daad4d485
ouwenfeng <834295030@qq.com>
2019-02-22 19:34:36 +0800

    feedback所有内容

--- rihanfalv/src/app/app.module.ts
+++ rihanfalv/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
@@ -11,6 +14,8 @@ import { CaseComponent } from './components/case/case.component';
 import { PersonpageComponent } from './components/personpage/personpage.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { HeadComponent } from './components/head/head.component';
+import { FeedbacksuccessComponent } from './components/Feedback/feedbacksuccess/feedbacksuccess.component';
+import { PersonalfeedbackComponent } from './components/Feedback/personalfeedback/personalfeedback.component';
 
 @NgModule({
   declarations: [
@@ -22,11 +27,15 @@ import { HeadComponent } from './components/head/head.component';
     CaseComponent,
     PersonpageComponent,
     ChatComponent,
-    HeadComponent
+    HeadComponent,
+    FeedbacksuccessComponent,
+    PersonalfeedbackComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
