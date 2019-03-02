9c908f616110bb857de25aec7ea2990da31904ac
BlackCarDriver <13169346002@163.com>
2019-03-02 19:47:12 +0800

    完善homepage前后端，停止使用服务器的数据，数据库建表和导入数据使用数据以及其他功能亲测可用

---
+++ golang/Sql/insert_data.sql
@@ -0,0 +1,32 @@
+--the following lines is use to create data for homepage test
+insert into homepagenews(imgurl,linkurl,title) values(
+    'getimage/?tag=&name=1.jpg',
+    'getimage/?tag=&name=1.jpg',
+    'getimage/?tag=&name=1.jpg'
+);
+insert into homepagenews(imgurl,linkurl,title) values(
+    'getimage/?tag=&name=2.jpg',
+    'getimage/?tag=&name=2.jpg',
+    'getimage/?tag=&name=2.jpg'
+);
+insert into homepagenews(imgurl,linkurl,title) values(
+    'getimage/?tag=&name=3.jpg',
+    'getimage/?tag=&name=3.jpg',
+    'getimage/?tag=&name=3.jpg'
+);insert into homepagenews(imgurl,linkurl,title) values(
+    'getimage/?tag=&name=4.jpg',
+    'getimage/?tag=&name=4.jpg',
+    'getimage/?tag=&name=4.jpg'
+);
+insert into homepagenews(imgurl,linkurl,title) values(
+    'getimage/?tag=&name=5.jpg',
+    'getimage/?tag=&name=5.jpg',
+    'getimage/?tag=&name=5.jpg'
+);
+insert into homepageartical(imgurl,linkurl,brif,date)values(
+    'getimage/?tag=&name=11.jpg',
+    'getimage/?tag=&name=11.jpg',
+	'只要angular更golang在同一个地址上这些图片就可以加载到了',
+    CURRENT_DATE
+);
+--homepage test data over...
