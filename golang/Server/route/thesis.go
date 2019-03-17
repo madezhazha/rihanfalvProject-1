3608ebc6fef5ddc39adf907654bb59f8b8d7eeca
kongzhou <715536017@qq.com>
2019-03-17 18:52:45 +0800

    thesis bug

--- golang/Server/route/thesis.go
+++ golang/Server/route/thesis.go
@@ -2,37 +2,37 @@ package route
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
-	"../psql"
 	"strconv"
 	"time"
+
+	"../psql"
 )
 
 //用于按信息查询数据库文章列表
-type Mes struct{
-	CurrentPage int;  //当前文章列表页数最后文章Id
-	Country string;  //当前模块日/韩  
+type Mes struct {
+	CurrentPage int    //当前文章列表页数最后文章Id
+	Country     string //当前模块日/韩
 }
 
 //按信息查询文章详情
-type Detial struct{
-	UserID string;
-	ArticleID string;
-	Country string;
+type Detial struct {
+	UserID    string
+	ArticleID string
+	Country   string
 }
 
 //处理收藏信息结构体
-type Collection struct{
-	UserID string
-	ArticleID string
-	Country string
+type Collection struct {
+	UserID      string
+	ArticleID   string
+	Country     string
 	IsCollected bool
 }
 
 //显示论文列表上传
-func ArticleList(w http.ResponseWriter, r *http.Request){
+func ArticleList(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问..域  跨域
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
 	w.Header().Set("content-type", "application/json")             //返回数据格式是json
@@ -40,23 +40,23 @@ func ArticleList(w http.ResponseWriter, r *http.Request){
 	body, _ := ioutil.ReadAll(r.Body)
 	var Articles []psql.Article //文章结构体数组
 	var mes Mes
-	json.Unmarshal(body,&mes)
-	if mes.Country==""{
+	json.Unmarshal(body, &mes)
+	if mes.Country == "" {
 		return
 	}
 
-	if mes.Country=="Japan"{
-		Articles=psql.GetJapanArticlesPart(mes.CurrentPage-1)
-	}else if mes.Country=="Korea"{
-		Articles=psql.GetKoreaArticlesPart(mes.CurrentPage-1)
+	if mes.Country == "Japan" {
+		Articles = psql.GetJapanArticlesPart(mes.CurrentPage - 1)
+	} else if mes.Country == "Korea" {
+		Articles = psql.GetKoreaArticlesPart(mes.CurrentPage - 1)
 	}
-	data,_:=json.Marshal(Articles) 
+	data, _ := json.Marshal(Articles)
 	w.Write(data)
 	//fmt.Println("submit Articles sucess")
 }
 
 //获取文章详情
-func ArticleDetial(w http.ResponseWriter, r *http.Request){
+func ArticleDetial(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问..域  跨域
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
 	w.Header().Set("content-type", "application/json")             //返回数据格式是json
@@ -64,65 +64,65 @@ func ArticleDetial(w http.ResponseWriter, r *http.Request){
 	body, _ := ioutil.ReadAll(r.Body)
 	var detial Detial
 	//var collletion Collection
-	json.Unmarshal(body,&detial)  //collletion
+	json.Unmarshal(body, &detial) //collletion
 	//id传 0 bug
-	if detial.Country==""{     //collletion.ArticleID==0
+	if detial.Country == "" { //collletion.ArticleID==0
 		return
 	}
 	//fmt.Println(detial.Country)
 	var article psql.Article
-	articleid,_:=strconv.Atoi(detial.ArticleID)
-	userid,_:=strconv.Atoi(detial.UserID)
-	if detial.Country=="Japan"{
-		article=psql.GetJapanArticle(articleid)  //collletion.ArticleID
+	articleid, _ := strconv.Atoi(detial.ArticleID)
+	userid, _ := strconv.Atoi(detial.UserID)
+	if detial.Country == "Japan" {
+		article = psql.GetJapanArticle(articleid) //collletion.ArticleID
 		//判断收藏
-		if userid!=0{
-			article.IsCollected=psql.GetCollectedArticle(userid,"japanthesis",articleid)
+		if userid != 0 {
+			article.IsCollected = psql.GetCollectedArticle(userid, "japanthesis", articleid)
 		}
-	}else if detial.Country=="Korea"{
-		article=psql.GetKoreaArticle(articleid)
-		if userid!=0{
-			article.IsCollected=psql.GetCollectedArticle(userid,"koreathesis",articleid)
+	} else if detial.Country == "Korea" {
+		article = psql.GetKoreaArticle(articleid)
+		if userid != 0 {
+			article.IsCollected = psql.GetCollectedArticle(userid, "koreathesis", articleid)
 		}
 	}
 
-	data,_:=json.Marshal(article)
+	data, _ := json.Marshal(article)
 	w.Write(data)
 	//fmt.Println("submit article sucess")
-	
+
 }
 
 //处理文章收藏和取消收藏
-func IsCollectedArticle(w http.ResponseWriter, r *http.Request){
+func IsCollectedArticle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问..域  跨域
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
 	w.Header().Set("content-type", "application/json")             //返回数据格式是json
 	defer r.Body.Close()
 	body, _ := ioutil.ReadAll(r.Body)
 	var collection Collection
-	json.Unmarshal(body,&collection)
-	if collection.Country==""{
+	json.Unmarshal(body, &collection)
+	if collection.Country == "" {
 		return
 	}
-	articleid,_:=strconv.Atoi(collection.ArticleID)
-	userid,_:=strconv.Atoi(collection.UserID)
-	if collection.IsCollected==true{
-		if collection.Country=="Japan"{
-			psql.DeleteArticleCollect(userid,"japanthesis",articleid)
-		}else if collection.Country=="Korea"{
-			psql.DeleteArticleCollect(userid,"koreathesis",articleid)
+	articleid, _ := strconv.Atoi(collection.ArticleID)
+	userid, _ := strconv.Atoi(collection.UserID)
+	if collection.IsCollected == true {
+		if collection.Country == "Japan" {
+			psql.DeleteArticleCollect(userid, "japanthesis", articleid)
+		} else if collection.Country == "Korea" {
+			psql.DeleteArticleCollect(userid, "koreathesis", articleid)
 		}
-	}else if collection.IsCollected==false{
-		if collection.Country=="Japan"{
-			t:=time.Now().Format("2006-01-02")
-			psql.CollectArticle(userid,"japanthesis",articleid,t)
-		}else if collection.Country=="Korea"{
-			t:=time.Now().Format("2006-01-02")
-			psql.CollectArticle(userid,"koreathesis",articleid,t)
+	} else if collection.IsCollected == false {
+		if collection.Country == "Japan" {
+			t := time.Now().Format("2006-01-02")
+			psql.CollectArticle(userid, "japanthesis", articleid, t)
+		} else if collection.Country == "Korea" {
+			t := time.Now().Format("2006-01-02")
+			psql.CollectArticle(userid, "koreathesis", articleid, t)
 		}
 	}
-	var result map[string]int=map[string]int{"Result":1}
-	date,_:=json.Marshal(result)
+	var result map[string]int = map[string]int{"Result": 1}
+	date, _ := json.Marshal(result)
 	w.Write(date)
 
 }
